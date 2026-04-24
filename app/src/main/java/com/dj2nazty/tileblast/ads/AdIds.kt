3cf37630f03c5d4e5384d32f86539eaa7e3b830b
dj2nazty <dj2nazty1@gmail.com>
2026-04-24 08:36:46 -0400

    Use real AdMob Banner ad unit ID

--- app/src/main/java/com/dj2nazty/tileblast/ads/AdIds.kt
+++ app/src/main/java/com/dj2nazty/tileblast/ads/AdIds.kt
@@ -8,7 +8,7 @@ package com.dj2nazty.tileblast.ads
  * The AdMob APPLICATION_ID lives in AndroidManifest.xml (also a test id today).
  */
 object AdIds {
-    const val BANNER = "ca-app-pub-3940256099942544/6300978111"
+    const val BANNER = "ca-app-pub-4183870506667640/9033317879"
     const val INTERSTITIAL = "ca-app-pub-3940256099942544/1033173712"
     const val REWARDED = "ca-app-pub-3940256099942544/5224354917"
 }
